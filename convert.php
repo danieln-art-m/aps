9cc36ecb85bf07deff64611553f4a694e2c6fb7f
Daniel Oliveira <danielffm@hotmail.com>
2024-06-12 20:42:06 +0200

    Kommetare entfernt

--- convert.php
+++ convert.php
@@ -5,12 +5,12 @@ require('vendor/autoload.php');
 class Converter{
 
     private $freecurrencyapi;
-
+//------------------------------------------------------------
     public function __construct(){
 
            $this->freecurrencyapi = new \FreeCurrencyApi\FreeCurrencyApi\FreeCurrencyApiClient('fca_live_N7le7zZ19RP080HibBba4NSTb8bUYvOjYXQ5G2KN'); 
     }
-
+//-------------------------------------------------------------
     public function convert($from, $to){
 
         $result =  $this->freecurrencyapi->latest([
@@ -21,7 +21,7 @@ class Converter{
         return "1 ".$from ." beträgt ".$result['data'][$to]." ". $to;
 
     }
-
+//-------------------------------------------------------------
     public function getCurrencies(){
 
         return $this->freecurrencyapi->currencies();
@@ -31,6 +31,3 @@ class Converter{
 
 }
 
-//-------------------------------------------------
-//$test = new Converter();
-//echo json_encode($test->getCurrencies());
